55b62c4c16b2e1a59a28f4476f164ae6d7632bf5
stuckenberg <github@stuckenberg.de>
2014-03-16 20:23:32 +0100

    added timeline

--- js/custom.js
+++ js/custom.js
@@ -1342,9 +1342,28 @@ $(window).on("resize",function(e){
 });
 
          
+/*
+|--------------------------------------------------------------------------
+| TIMELINE
+|--------------------------------------------------------------------------
+*/
 
+$(document).ready(function(){
+	var my_posts = $("[rel=tooltip]");
 
+	var size = $(window).width();
+	for(i=0;i<my_posts.length;i++){
+		the_post = $(my_posts[i]);
 
+		if(the_post.hasClass('invert') && size >=767 ){
+			the_post.tooltip({ placement: 'left'});
+			the_post.css("cursor","pointer");
+		}else{
+			the_post.tooltip({ placement: 'rigth'});
+			the_post.css("cursor","pointer");
+		}
+	}
+});
 
 
 
